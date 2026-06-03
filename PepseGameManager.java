e57f09beaaf0b75a3464b66c9e75cfaec121028e
Diana Basil <163316796+basildiana@users.noreply.github.com>
2026-06-03 13:43:51 +0300

    block logic + terrain logic + changes in game manager to create terrain in game

--- PepseGameManager.java
+++ PepseGameManager.java
@@ -6,11 +6,15 @@ import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import danogl.util.Vector2;
+import pepse.world.Block;
 import pepse.world.Sky;
+import pepse.world.Terrain;
+import java.util.List;
 
 public class PepseGameManager extends GameManager {
-
-
+    // Constant initialization configurations
+    private static final int INIT_SEED = 1000; // for us to play (changes ground)
+    private static final int START_X = 0;
     @Override
     public void initializeGame(ImageReader imageReader,
                                SoundReader soundReader,
@@ -19,11 +23,29 @@ public class PepseGameManager extends GameManager {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         Vector2 windowDimensions = windowController.getWindowDimensions();
 
-        // create sky
+
+        createSky(windowDimensions); // create sky
+        createTerrain(windowDimensions); // create gound
+
+
+    }
+    /** creates the sky object and adds it to the back layer  */
+    private void createSky(Vector2 windowDimensions) {
         GameObject sky = Sky.create(windowDimensions);
         int skyLayer = Layer.BACKGROUND;
         gameObjects().addGameObject(sky, skyLayer);
     }
+
+    /**creates the ground blocks and adds them to the static layer */
+    private void createTerrain(Vector2 windowDimensions) {
+        Terrain terrain = new Terrain(windowDimensions, INIT_SEED);
+        int maxX = (int) windowDimensions.x();
+        List<Block> blocksList = terrain.createInRange(START_X, maxX);
+        for (Block block: blocksList) {
+            gameObjects().addGameObject(block, Layer.STATIC_OBJECTS);
+        }
+    }
+
 
     public static void main(String[] args) {
         new PepseGameManager().run();
--- pepse/world/Block.java
+++ pepse/world/Block.java
@@ -1,4 +1,23 @@
 package pepse.world;
 
-public class Block {
+import danogl.GameObject;
+import danogl.components.GameObjectPhysics;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+/**
+ * Represents a single block unit, extends GameObject.
+ * @author Diana Basil and Meital Lubarski
+ */
+public class Block extends GameObject {
+    /** pixel dimensions (width and height) of a single block */
+    public static final int SIZE = 30;
+    /** Constructs a new Block instance at a position.
+     * @param topLeftCorner The coordinates for the top-left of the block.
+     * @param renderable    The visual asset or color to render for this block.
+     */
+    public Block(Vector2 topLeftCorner, Renderable renderable) {
+        super(topLeftCorner, Vector2.ONES.mult(SIZE), renderable);
+        physics().preventIntersectionsFromDirection(Vector2.ZERO);
+        physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
+    }
 }
