d0dc884d3b7066cf353f98eded9e08af6d3c6044
Diana Basil <163316796+basildiana@users.noreply.github.com>
2026-06-03 10:56:47 +0300

    sky obj + game manager adding the sku obj

--- PepseGameManager.java
+++ PepseGameManager.java
@@ -1,25 +1,31 @@
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import danogl.util.Vector2;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
 
-    public PepseGameManager() {
-        super("Pepse Game", new Vector2(1200, 700));
-    }
 
     @Override
     public void initializeGame(ImageReader imageReader,
                                SoundReader soundReader,
                                UserInputListener inputListener,
                                WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Vector2 windowDimensions = windowController.getWindowDimensions();
+
+        // create sky
+        GameObject sky = Sky.create(windowDimensions);
+        int skyLayer = Layer.BACKGROUND;
+        gameObjects().addGameObject(sky, skyLayer);
     }
 
     public static void main(String[] args) {
-        // new PepseGameManager().run();
         new PepseGameManager().run();
     }
 }
