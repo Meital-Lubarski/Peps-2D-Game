0590ea1e4eb47f115dc0b4ccf4fde786b721f12b
Meital-Lubarski <163754747+Meital-Lubarski@users.noreply.github.com>
2026-06-04 11:22:29 +0300

    Added sun and sun halo

--- PepseGameManager.java
+++ PepseGameManager.java
@@ -10,6 +10,8 @@ import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
 import java.util.List;
+import pepse.world.daynight.Sun;
+import pepse.world.daynight.SunHalo;
 
 import pepse.world.daynight.Night;
 
@@ -31,10 +33,18 @@ public class PepseGameManager extends GameManager {
 
         createSky(windowDimensions); // create sky
         createTerrain(windowDimensions); // create gound
+        createSunAndHalo(windowDimensions);
         createNight(windowDimensions);
 
-
     }
+
+    private void createSunAndHalo(Vector2 windowDimensions){
+        GameObject sun = Sun.create(windowDimensions, DAY_CYCLE);
+        GameObject sunHalo = SunHalo.create(sun);
+        gameObjects().addGameObject(sunHalo, Layer.BACKGROUND + 1);
+        gameObjects().addGameObject(sun, Layer.BACKGROUND + 2);
+    }
+
     /** creates the sky object and adds it to the back layer  */
     private void createSky(Vector2 windowDimensions) {
         GameObject sky = Sky.create(windowDimensions);
