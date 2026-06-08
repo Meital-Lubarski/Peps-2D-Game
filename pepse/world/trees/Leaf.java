4a4e7bdb44a3fefd4e6cd37febe4b724708888b1
Meital-Lubarski <163754747+Meital-Lubarski@users.noreply.github.com>
2026-06-08 20:42:02 +0300

    Add leaf width animation

--- pepse/world/trees/Leaf.java
+++ pepse/world/trees/Leaf.java
@@ -18,6 +18,8 @@ public class Leaf {
     private static final float MAX_ANGLE = 12f;
     private static final float WIND_TIME = 1.5f;
     private static final float MAX_DELAY = 1f;
+    private static final float MIN_WIDTH = Block.SIZE * 0.9f;
+    private static final float MAX_WIDTH = Block.SIZE * 1.1f;
 
     /**
      * Prevents instances.
@@ -35,10 +37,21 @@ public class Leaf {
 
     private static void windAnimation(GameObject leaf, Random random) {
         float delay = random.nextFloat() * MAX_DELAY;
-        new ScheduledTask(leaf, delay, false, () -> addAngeTransition(leaf));
+        new ScheduledTask(leaf, delay, false, () -> addWindTransitions(leaf));
     }
 
-    private static void addAngeTransition(GameObject leaf) {
+    private static void addWindTransitions(GameObject leaf) {
+        addAngleTransition(leaf);
+        addWidthTransition(leaf);
+    }
+
+    private static void addWidthTransition(GameObject leaf) {
+        new Transition<>(leaf, leaf::setDimensions, new Vector2(MIN_WIDTH, Block.SIZE),
+                new Vector2(MAX_WIDTH, Block.SIZE), Transition.LINEAR_INTERPOLATOR_VECTOR, WIND_TIME,
+                Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null);
+    }
+
+    private static void addAngleTransition(GameObject leaf) {
         new Transition<>(leaf, leaf.renderer()::setRenderableAngle, -MAX_ANGLE, MAX_ANGLE,
                 Transition.LINEAR_INTERPOLATOR_FLOAT, WIND_TIME,
                 Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null);
